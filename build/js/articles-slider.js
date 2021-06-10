fb68d00abd213f0a15c57ccc603a1229cf91a5b5
mazax1ct <vova@giveandget.ru>
2021-06-10 15:03:43 +0300

    news on main

---
+++ build/js/articles-slider.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  if($('.js-articles-slider').length) {
+    $('.js-articles-slider').each(function(index) {
+      $(this).slick({
+        arrows: true,
+        prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_left"/></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_right"/></svg></button>',
+        mobileFirst: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0px',
+        asNavFor: $('.js-articles-slider-nav[data-id="'+$(this).attr('data-id')+'"]'),
+        responsive: [
+          {
+            breakpoint: 1023,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    });
+  }
+
+  if($('.js-articles-slider-nav').length) {
+    $('.js-articles-slider-nav').each(function(index) {
+      $(this).slick({
+        arrows: true,
+        prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_left"/></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_right"/></svg></button>',
+        mobileFirst: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0px',
+        focusOnSelect: false,
+        draggable: false,
+        swipe: false,
+        asNavFor: $('.js-articles-slider[data-id="'+$(this).attr('data-id')+'"]')
+      });
+    });
+  }
+});
---
+++ src/js/news-slider.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  if($('.js-news-slider').length) {
+    $('.js-news-slider').each(function(index) {
+      $(this).slick({
+        arrows: false,
+        mobileFirst: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '20px',
+        //asNavFor: $('.js-news-slider-nav[data-id="'+$(this).attr('data-id')+'"]'),
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              arrows: true,
+              prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_left"/></svg></button>',
+              nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_right"/></svg></button>',
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              centerPadding: '60px'
+            }
+          },
+          {
+            breakpoint: 1899,
+            settings: {
+              arrows: true,
+              prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_left"/></svg></button>',
+              nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_right"/></svg></button>',
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              centerPadding: '128px'
+            }
+          },
+        ]
+      });
+    });
+  }
+
+  if($('.js-news-slider-nav').length) {
+    $('.js-news-slider-nav').each(function(index) {
+      $(this).slick({
+        arrows: true,
+        prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_left"/></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#chevron_right"/></svg></button>',
+        mobileFirst: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        centerMode: true,
+        centerPadding: '0px',
+        focusOnSelect: false,
+        draggable: false,
+        swipe: false,
+        asNavFor: $('.js-news-slider[data-id="'+$(this).attr('data-id')+'"]')
+      });
+    });
+  }
+});
