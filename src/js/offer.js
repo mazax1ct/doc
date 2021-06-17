b98ce2d094c7c72dc33ab8fe8964eed1f11e0792
mazax1ct <vova@giveandget.ru>
2021-06-17 12:29:12 +0300

    articles + news + links

--- src/js/offer.js
+++ src/js/offer.js
@@ -1,4 +1,4 @@
-$(document).on('click', '.offer__block-toggler button', function () {
+$(document).on('click', '.offer__toggler button', function () {
   if($('body').width() < 1024) {
     $(this).toggleClass('is-active');
     $(this).parent().next('.offer__text').slideToggle();
